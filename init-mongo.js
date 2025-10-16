e9e7ab6d955dc53d5719254465c5360d010c744c
Dmitry Ivanov <darcenrall@gmail.com>
2025-10-16 10:26:05 +0300

    update test configuration

--- init-mongo.js
+++ init-mongo.js
@@ -1,4 +1,5 @@
-db = db.getSiblingDB('moodle-statistics');
+const db_name = process.env.MONGODB_DATABASE
+db = db.getSiblingDB(db_name);
 
 // set your users
 db.users.insertMany([
@@ -11,3 +12,77 @@ db.users.insertMany([
         "password": "bhewrtfm3klmt3"
     }
 ])
+
+
+if (process.env.TEST_RUN == 'true'){
+    // set test statistic data
+    db.sessions.insertMany([
+        {
+            "_id": ObjectId("6648756f964c1253e72202db"),
+            "browser": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit",
+            "page": "http://e.moevm.info",
+            "page_html": "<html></html>",
+            "title": "kakoy-to title",
+            "window": {
+                "height": 800,
+                "width": 1200
+            }
+        },
+        {
+            "_id": ObjectId("6648756f964c1253e72202dc"),
+            "browser": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit",
+            "page": "http://e.moevm.info",
+            "page_html": "<html></html>",
+            "title": "kakoy-to title",
+            "window": {
+                "height": 800,
+                "width": 1200
+            }
+        }
+    ])
+
+    db.statistics.insertMany([
+        {
+            "student_id": 2071,
+            "student": "Диана Романенко",
+            "session_id": ObjectId("6648756f964c1253e72202db"),
+            "email": "diana@mail.ru",
+            "course": "Курс молодого бойца",
+            "actions": [
+                {
+                    "timestamp": new Date("2024-02-02T14:00:00"),
+                    "element_type": "button",
+                    "element_name": "сохранить",
+                    "action_type": "conversation",
+                    "event_type": "mousedown", 
+                    "element_html": `<input id="id_q_6627c493e6c5f" class="form-control" type="text" name="q" placeholder="Поиск" size="13" tabindex="-1">`
+                }
+            ]
+        },
+        {
+            "student_id": 824,        
+            "student": "Беззубов Даниил",
+            "email": "daniil@mail.ru",
+            "session_id": ObjectId("6648756f964c1253e72202dc"),
+            "course": "Курс молодого бойца",
+            "actions": [
+                {
+                    "timestamp": new Date("2024-02-02T14:00:00"),
+                    "element_type": "button",
+                    "element_name": "сохранить",
+                    "action_type": "conversation",
+                    "event_type": "mousedown", 
+                    "element_html": `<input id="id_q_6627c493e6c5f" class="form-control" type="text" name="q" placeholder="Поиск" size="13" tabindex="-1">`
+                },
+                {
+                    "timestamp": new Date("2024-02-02T14:00:00"),
+                    "element_type": "button",
+                    "element_name": "следующая страница",
+                    "action_type": "conversation",
+                    "event_type": "mousedown", 
+                    "element_html": `<input id="id_q_6627c493e6c5f" class="form-control" type="text" name="q" placeholder="Поиск" size="13" tabindex="-1">`
+                }
+            ]
+        }
+    ])
+}
