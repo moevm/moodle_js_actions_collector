2be380991d93d84ae1799a04b886d773e6b08327
Shalaginov Ivan <vaneck2003@gmail.com>
2025-12-05 14:55:31 +0300

    fix test_statistics_repo.py

--- server/tests/test_statistics_repo.py
+++ server/tests/test_statistics_repo.py
@@ -1,4 +1,4 @@
-from unittest.mock import Mock
+from unittest.mock import Mock, AsyncMock
 import pytest
 from src.core.modules.database.statistics import MongoStatisticRepo
 from src.models.filter import SessionFilter
@@ -9,7 +9,11 @@ from src.core.modules.database.errors import RepoNotFoundError
 async def test_get_all_sessions():
     # Arrange
     mock_client = Mock()
-    mock_client.statistics.find.return_value.sort.return_value.skip.return_value.limit.return_value.to_list.return_value = find_to_list()
+    mock_client.statistics = Mock()
+    mock_client.statistics.count_documents = AsyncMock(return_value=100)
+    mock_client.statistics.find.return_value.sort.return_value.skip.return_value.limit.return_value.to_list = AsyncMock(
+        return_value=find_to_list()
+    )
     mock_filter = create_mock_filter()
 
     repository = MongoStatisticRepo(mock_client)
@@ -142,7 +146,7 @@ def create_mock_filter():
     return mock_filter
 
 
-async def find_to_list():
+def find_to_list():
     return [{
         "_id": "1", "session_id": "2", "actions": [{
             "timestamp": "2024-02-02T00:01",
